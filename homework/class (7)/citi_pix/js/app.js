f117ada34cde5ed118eb854754a62c1c3a3d8b50
Janine <jredick77@gmail.com>
2018-05-13 16:24:26 -0400

    April and May Classes

---
+++ homework/class (7)/citi_pix/js/app.js
@@ -0,0 +1,54 @@
+// As a user
+// When I change my selection
+// I expect the background image to change into the image of my selection
+//here's my pseudo code
+	//click on dropdown, see list of cities
+	//take user select
+	//search for city variable, check if string has a match
+	//display associated image
+	//reset select
+
+$(document).ready(function() {
+
+  // begin doc ready
+
+//dropdown of cities
+var citySelect = ['nyc', 'sf', 'la', 'atx', 'syd'];
+for(i=0; i<citySelect.length; i++){
+	console.log(citySelect[i]);
+	$('#city-select').append("<option>"+citySelect[i]+'</option>');  
+}
+
+	//set var city, take input
+	$("#city-select").change(function() {
+
+	var city = $('#city-select').val();
+	console.log(city);
+
+	//checks string, displays image
+
+		if (city === 'nyc'){		
+				$('body').attr('class','nyc');
+		
+				
+		} else if (city === 'sf'){
+				$('body').attr('class','sf');
+			
+
+		} else if (city === 'la'){
+				$('body').attr('class','la');
+			
+
+		} else if (city === "atx"){
+				$('body').attr('class','austin');
+		
+
+		} else if (city === "syd"){
+				$('body').attr('class','sydney');
+
+		}
+
+//end doc ready
+});	
+
+});
